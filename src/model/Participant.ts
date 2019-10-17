3c365561cfc5637aadae1e3cd8b592163b44a5fa
Sebastian Lang <admin@sebastian-lang.eu>
2019-10-18 01:59:16 +0200

    Add pouchdb + data model

---
+++ src/model/Participant.ts
@@ -0,0 +1,7 @@
+export interface Participant {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    birthdate: Date;
+    hasPassport: boolean;
+}
---
+++ src/model/Prize.ts
@@ -0,0 +1,8 @@
+export interface Prize {
+    _id: string;
+    name: string;
+    participants: number;
+    minAge: number;
+    maxAge: number;
+    location: string;
+}
